3fad03aa99b52b21f622353b3968f2fc95c51ac6
Andrew White <aeewhite@gmail.com>
2016-10-03 16:08:37 -0500

    Edit todayview description

--- todayview.rb
+++ todayview.rb
@@ -1,5 +1,5 @@
 class Todayview < Formula
-  desc "View the days events from OS X System Calendar"
+  desc "View the day's events from OS X System Calendar"
   homepage "https://github.com/aeewhite/todayview"
   url "https://github.com/aeewhite/todayview/archive/0.4.tar.gz"
   sha256 "f85fc234ccc8b59e075e4e94d00af88814cd31327f7948dd268fecf659f73438"
