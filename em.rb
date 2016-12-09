dc806e1d50343b263040c5acf9c8887beaafc8f2
Andrew White <aeewhite@gmail.com>
2016-12-09 00:55:10 -0600

    Update Em to 1.1.0

--- em.rb
+++ em.rb
@@ -1,9 +1,9 @@
 class Em < Formula
   desc "An interpreted, dynamically typed language written in Java"
   homepage "https://github.com/aeewhite/Em"
-  url "https://github.com/aeewhite/Em/archive/1.0.0.tar.gz"
+  url "https://github.com/aeewhite/Em/archive/1.1.0.tar.gz"
   head "https://github.com/aeewhite/Em.git"
-  sha256 "142fbd166c9e5faad352d5d2ca55b1594fed2221abb62c16d1e6fef34e39144c"
+  sha256 "8790587533fcc51eb06261d72b03b9548e861a82d7577ea9a1075fd36697ef56"
 
   depends_on java: '>=8'
 
