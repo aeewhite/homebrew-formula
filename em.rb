fbeed307760fef712b19f3ee3683886d1d0356a9
Andrew White <aeewhite@gmail.com>
2016-12-08 22:35:26 -0600

    Add formula for Em

---
+++ em.rb
@@ -0,0 +1,18 @@
+class Em < Formula
+  desc "An interpreted, dynamically typed language written in Java"
+  homepage "https://github.com/aeewhite/Em"
+  url "https://github.com/aeewhite/Em/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "142fbd166c9e5faad352d5d2ca55b1594fed2221abb62c16d1e6fef34e39144c"
+
+  depends_on java: '>=8'
+
+  def install
+    system "make", "dist"
+    bin.install "dist/emi"
+  end
+
+  test do
+    system "emi -h"
+  end
+end
