d9f97f0b0064cf9cf5b1ae45d1f4ffe9f162929b
Andrew White <aeewhite@gmail.com>
2016-12-08 22:40:47 -0600

    Add head version to Em

--- em.rb
+++ em.rb
@@ -2,7 +2,7 @@ class Em < Formula
   desc "An interpreted, dynamically typed language written in Java"
   homepage "https://github.com/aeewhite/Em"
   url "https://github.com/aeewhite/Em/archive/1.0.0.tar.gz"
-  version "1.0.0"
+  head "https://github.com/aeewhite/Em.git"
   sha256 "142fbd166c9e5faad352d5d2ca55b1594fed2221abb62c16d1e6fef34e39144c"
 
   depends_on java: '>=8'
@@ -13,6 +13,6 @@ class Em < Formula
   end
 
   test do
-    system "emi -h"
+    system "#{bin}/emi -h"
   end
 end
